df51489c7f02832b1ee1861b367195c0029baa33
Jyoti <jyoti@cnergee.com>
2020-09-17 18:48:23 +0530

    Initial commit

---
+++ app/src/main/java/com/cnergee/mypage/obj/AuthenticationMobile.java
@@ -0,0 +1,8 @@
+package com.cnergee.mypage.obj;
+
+public class AuthenticationMobile {
+
+	public static String CliectAccessId="CM0213SHR";//CM000111SC   CM000161SC     CM000194GB
+	public static String CliectAccessName="CliectAccessId";
+	public static String PROJECT_NUMBER="546201232020";//1042877903452
+}
